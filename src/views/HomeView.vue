64515865de33142965441344567d4186eb2c4f5a
Artem <mstrjesm@gmail.com>
2024-08-05 18:46:46 +0300

    feat(home-view): add test for base-pagination

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,12 +5,27 @@
       Home page
     </BaseButton>
     <BaseButton @click="isLoading = !isLoading">Click</BaseButton>
+    <BasePagination
+      :total-pages="11"
+      :total="113"
+      :per-page="10"
+      :current-page="currentPage"
+      @pagechanged="onPageChange"
+    />
   </main>
 </template>
 
 <script setup lang="ts">
 import BaseButton from '@/components/shared/BaseButton.vue';
+import BasePagination from '@/components/shared/BasePagination.vue';
 import { ref } from 'vue';
 
 const isLoading = ref(false);
+const currentPage = ref(1);
+
+const onPageChange = (page: number) => {
+  console.log(page);
+  currentPage.value = page;
+}
+
 </script>
