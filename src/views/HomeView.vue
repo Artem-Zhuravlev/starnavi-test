8f61b883cfec5cb687868e22ce49f6f68496cb1b
Artem <mstrjesm@gmail.com>
2024-08-05 16:51:56 +0300

    feat(home-view): add test buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,16 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <main>
-    Home page
+    {{ isLoading }}
+    <BaseButton :loading="isLoading">
+      Home page
+    </BaseButton>
+    <BaseButton @click="isLoading = !isLoading">Click</BaseButton>
   </main>
 </template>
+
+<script setup lang="ts">
+import BaseButton from '@/components/shared/BaseButton.vue';
+import { ref } from 'vue';
+
+const isLoading = ref(false);
+</script>
