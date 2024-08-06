2e18a6280f9d104d88e6387c507d859b64e97d51
Artem <mstrjesm@gmail.com>
2024-08-06 18:34:30 +0300

    feat(base-skeleton): add component

---
+++ src/components/shared/BaseSkeleton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="skeleton"
+    :style="{
+      width: `${props.width}px`,
+      height: `${props.height}px`
+    }"
+  />
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  width: number
+  height: number
+}>()
+</script>
+
+<style scoped lang="scss">
+.skeleton {
+  background: linear-gradient(
+    100deg,
+    rgba(255, 255, 255, 0) 40%,
+    rgba(255, 255, 255, .5) 50%,
+    rgba(255, 255, 255, 0) 60%
+  ) var(--color-gray);
+  background-size: 200% 100%;
+  background-position-x: 180%;
+  animation: 1s loading ease-in-out infinite;
+  border-radius: var(--interface-size-sm);
+}
+
+@keyframes loading {
+  to {
+    background-position-x: -20%;
+  }
+}
+
+</style>
