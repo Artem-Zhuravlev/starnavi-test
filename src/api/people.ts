5b357e215e65ba375bba1261e1103a454aafc596
Artem <mstrjesm@gmail.com>
2024-08-07 12:25:30 +0300

    feat(people.ts): add GET endpoint

---
+++ src/api/people.ts
@@ -0,0 +1,27 @@
+import axios, { type AxiosResponse } from 'axios'
+import type IPeople from '@/interfaces/IPeople'
+
+const url = import.meta.env.VITE_APP_SW_API_URL as string
+export const imageUrl = import.meta.env.VITE_APP_SW_IMAGES_URL as string
+
+interface IPeopleResponse {
+  count: number
+  next: string | null
+  previous: string | null
+  results: IPeople[]
+}
+
+export const getPeople = async (page?: number): Promise<IPeopleResponse> => {
+  try {
+    const response: AxiosResponse<IPeopleResponse> = await axios.get(`${url}/people`, {
+      params: {
+        page
+      }
+    })
+
+    return response.data
+  } catch (error) {
+    console.error('An error occurred while fetching people:', error)
+    throw error
+  }
+}
