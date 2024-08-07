90cfb82f710b3918a13f812d80c5af6a20707b91
Artem <mstrjesm@gmail.com>
2024-08-07 16:19:39 +0300

    feat(navbar): add component

---
+++ src/components/widgets/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar container-fluid">
+    <router-link class='navbar__logo'>
+      <img
+        src="/sw-logo.png"
+        alt="Star wars logo"
+      >
+    </router-link>
+  </nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang='scss'>
+.navbar {
+  display: flex;
+  align-items: center;
+  padding: var(--space-sm) var(--space-md);
+  background-color: var(--color-primary);
+  &__logo {
+    display: inline-flex;
+    max-width: 100px;
+  }
+}
+</style>
