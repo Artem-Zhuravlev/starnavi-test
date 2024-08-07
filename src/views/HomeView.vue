996cbe1861d3a78bb604340aa3b21f73b245ace1
Artem <mstrjesm@gmail.com>
2024-08-07 12:24:14 +0300

    feat(home-view): add people list, remove unused logic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,44 +1,12 @@
 <template>
   <main>
-    {{ isLoading }}
-    <BaseButton :loading="isLoading"> Home page </BaseButton>
-    <BaseButton @click="isLoading = !isLoading">Click</BaseButton>
-    <BasePagination
-      :total-pages="11"
-      :total="113"
-      :per-page="10"
-      :current-page="currentPage"
-      @page-changed="onPageChange"
-    />
-    <div style="display: grid; grid-template-columns: 1fr 1fr 1fr;">
-      <base-card
-        image='https://starwars-visualguide.com/assets/img/characters/2.jpg'
-        title="Gold"
-        date="2014-12-09T13:50:51.644000Z"
-        to="/"
-      />
-      <base-card
-        title="Gold"
-        date="2014-12-09T13:50:51.644000Z"
-        to="/"
-      />
-      <base-card-skeleton/>
+    <div class="container">
+      <people-list />
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import BaseButton from '@/components/shared/BaseButton.vue'
-import BasePagination from '@/components/shared/BasePagination.vue'
-import BaseCard from '@/components/shared/BaseCard.vue';
-import BaseCardSkeleton from '@/components/shared/BaseCardSkeleton.vue';
-import { ref } from 'vue'
+import PeopleList from '@/components/entities/people/PeopleList.vue';
 
-const isLoading = ref(false)
-const currentPage = ref(1)
-
-const onPageChange = (page: number) => {
-  console.log(page)
-  currentPage.value = page
-}
 </script>
