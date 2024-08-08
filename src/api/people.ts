b9243f98262e1616d75666be901cc6180f8ffdf4
Artem <mstrjesm@gmail.com>
2024-08-08 16:36:47 +0300

    feat(getPersonWithDetailsById): add endpoint

--- src/api/people.ts
+++ src/api/people.ts
@@ -1,5 +1,7 @@
 import axios, { type AxiosResponse } from './axios'
 import type IPeople from '@/interfaces/IPeople'
+import { getFilmById } from './films'
+import { getStarshipById } from './starship'
 
 export interface IPeopleResponse<T> {
   count: number
@@ -29,6 +31,38 @@ export const getPersonById = async (id: string): Promise<IPeople> => {
     return response.data
   } catch (error) {
     console.error('An error occurred while fetching person by id:', error)
+    throw error
+  }
+}
+
+export const getPersonWithDetailsById = async (id: string) => {
+  try {
+    const person = await getPersonById(id)
+
+    if (!person) {
+      throw new Error('Person not found')
+    }
+
+    const filmRequests = person.films ? person.films.flatMap((filmUrl) => getFilmById(filmUrl)) : []
+    const filmResponses = await Promise.all(filmRequests)
+    const films = filmResponses.map((response) => response)
+    const starshipRequests = person.starships
+      ? person.starships.map((starshipUrl) => getStarshipById(starshipUrl))
+      : []
+    const starshipResponses = await Promise.all(starshipRequests)
+    const starships = starshipResponses.map((response) => response)
+
+    return {
+      ...person,
+      films: films.map((film) => {
+        return {
+          title: film.title,
+          starships: starships.filter((starship) => film.starships.includes(starship.id))
+        }
+      })
+    }
+  } catch (error) {
+    console.error('An error occurred while fetching person details by id:', error)
     throw error
   }
 }
