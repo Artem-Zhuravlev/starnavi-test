1d7e8e8d0ff6e85647742f6bbaab327272d21712
Artem <mstrjesm@gmail.com>
2024-08-10 18:18:04 +0300

    feat(base-flow): add component

---
+++ src/components/shared/BaseFlow.vue
@@ -0,0 +1,33 @@
+<template>
+  <vue-flow
+    class="base-flow"
+    :nodes="props.nodes"
+    :edges="props.edges"
+    :default-viewport="{ zoom: 1.2 }"
+    :min-zoom="0.2"
+    :max-zoom="4"
+  />
+</template>
+
+<script setup lang="ts">
+import type { Node, Edge } from '@vue-flow/core';
+import { VueFlow } from '@vue-flow/core';
+
+// Define your nodes and edges data
+const props = defineProps<{
+  nodes: Node[];
+  edges: Edge[];
+}>();
+</script>
+
+<style>
+@import '@vue-flow/core/dist/style.css';
+@import '@vue-flow/core/dist/theme-default.css';
+
+.base-flow {
+  min-height: 400px;
+  width: 100%;
+  border: 2px solid var(--color-secondary);
+  background-color: var(--color-light-gray);
+}
+</style>
