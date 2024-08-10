0eff55e146585c832c19ddb9c04dbceb6c82a887
Artem <mstrjesm@gmail.com>
2024-08-10 18:59:39 +0300

    feat(person-details): add view

---
+++ src/views/PersonDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <div class="container">
+      <person-details-flow
+        v-if="personDetails"
+        :details="personDetails"
+      />
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import PersonDetailsFlow from '@/components/entities/people/PersonDetailsFlow.vue';
+import type { IPersonDetails } from '@/interfaces/IPersonDetails';
+import { onBeforeMount, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { getPersonWithDetailsById } from '@/api/people';
+
+const route = useRoute();
+const userId = route.params.id as string;
+const personDetails = ref<IPersonDetails | null>(null);
+
+onBeforeMount(async () => {
+  try {
+    const data = await getPersonWithDetailsById(userId);
+    personDetails.value = data as IPersonDetails;
+  } catch (error) {
+    console.error('Failed to fetch person details:', error);
+  }
+});
+</script>
